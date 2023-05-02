5583d075aa3aa56fe3746c95e2c4c19794ca4191
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-02 11:44:54 -0700

    Apply inter font to main content of site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,12 @@ import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 }
