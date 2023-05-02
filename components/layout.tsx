02ffae6e7c8e43d5aadc6ce9e532d2bff2a7c9eb
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-02 11:10:55 -0700

    Add layout component and update app definition

---
+++ components/layout.tsx
@@ -0,0 +1,20 @@
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Reviews</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+}
