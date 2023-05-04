343a158877904c11d7a6daf78297bc72699c9757
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-04 15:08:47 -0700

    Unused code

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,28 +1,4 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Switch } from "@headlessui/react";
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Contact() {
-  const [agreed, setAgreed] = useState(false);
-
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div
