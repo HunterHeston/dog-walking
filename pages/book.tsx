5fdb9b1cb00b0aa421b0b896932c6bbf319fb9a2
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-04 15:40:13 -0700

    Add tailwind form for drop in form. display forms based on selected option

--- pages/book.tsx
+++ pages/book.tsx
@@ -1,22 +1,43 @@
-import { FormEvent, useState } from "react";
+import DropInForm from "@/components/dropInForm";
+import OvernightForm from "@/components/overnightForm";
+import Select, { Option } from "@/components/tailwindui/select";
+import WalkForm from "@/components/walkForm";
 
-enum VisitType {
-  DropIn = "drop-in",
-  DogWalk = "dog-walk",
-  HouseSitting = "house-sitting",
-}
+import { useState } from "react";
+
+const walk: Option = {
+  id: 1,
+  name: "Dog Walk",
+};
+
+const dropIn: Option = {
+  id: 2,
+  name: "Drop In",
+};
+
+const overnight: Option = {
+  id: 3,
+  name: "Overnight",
+};
 
 export default function Contact() {
-  const [visitType, setVisitType] = useState<VisitType>(VisitType.DropIn);
+  const [selected, setSelected] = useState<Option>(walk);
 
   return (
     <>
-      <main className="py-24">
-        <select name="visit-type" id="visit-type">
-          <option value={VisitType.DropIn}>Drop-In Visit</option>
-          <option value={VisitType.DogWalk}>Dog Walk</option>
-          <option value={VisitType.HouseSitting}>House Sitting</option>
-        </select>
+      <main className="py-36 px-4 flex flex-col items-center">
+        <div className="sm:w-1/2">
+          <div className="mb-10">
+            <Select
+              options={[walk, dropIn, overnight]}
+              selected={selected}
+              setSelected={setSelected}
+            ></Select>
+          </div>
+          {selected.id == dropIn.id && <DropInForm></DropInForm>}
+          {selected.id == walk.id && <WalkForm></WalkForm>}
+          {selected.id == overnight.id && <OvernightForm></OvernightForm>}
+        </div>
       </main>
     </>
   );
