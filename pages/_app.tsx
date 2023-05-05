411282da4f6a87b64076b951ff576bb4d8e6d95c
Hunter Heston <huntersamuelheston@gmail.com>
2023-05-04 17:56:00 -0700

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <div className={inter.className}>
-        <Component {...pageProps} />
-      </div>
-    </Layout>
+    <>
+      <Layout>
+        <div className={inter.className}>
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+      <Analytics />
+    </>
   );
 }
