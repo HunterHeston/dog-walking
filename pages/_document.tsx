99087a288f0a12509014fb335555352d9ba87713
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-01 13:42:32 -0700

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Pet sitting services by Cheyenne Hawken."
+        />
+        <script
+          defer
+          data-domain="dog-walking.vercel.app"
+          src="https://plausible.io/js/script.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
